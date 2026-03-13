cd6d6d277cbb5b3e5c20593cdae3e706a43fa535
0steetime <tamakofan@aol.com>
2026-03-13 02:22:42 -0400

    started working on main view should've made a branch but i forgor

--- src/main/java/view/MainView.java
+++ src/main/java/view/MainView.java
@@ -1,4 +1,45 @@
 package view;
 
-public class MainView {
+import interface_adapter.quotes.QuotesController;
+import interface_adapter.quotes.QuotesViewModel;
+import interface_adapter.quotes.QuotesPresenter;
+import interface_adapter.quotes.QuotesState;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public class MainView extends JPanel implements ActionListener, PropertyChangeListener {
+
+    private final String viewName = "Main View";
+    private final QuotesViewModel quotesViewModel;
+
+    private final JButton icon;
+    private final JLabel date;
+    private final JLabel time;
+
+    private final JLabel quote;
+    private QuotesController quotesController;
+
+    public MainView(MainView mainView) {
+        this.quotesViewModel = mainView.quotesViewModel;
+        this.quotesViewModel.addPropertyChangeListener(this);
+
+    }
+
+    public MainView(QuotesViewModel quotesViewModel) {
+        this.quotesViewModel = quotesViewModel;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+
+    }
 }
